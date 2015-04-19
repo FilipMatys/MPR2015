af2007524d8eff0f89ccaa76bc3396245f97d1f5
Lorry1 <jlorenc01@gmail.com>
2015-04-19 20:23:14 +0200

    #31 Screen: Participations

---
+++ Excellent.HTMLClient/Screens/Participations/ViewParticipation.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewParticipation.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Participation.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Participation." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
