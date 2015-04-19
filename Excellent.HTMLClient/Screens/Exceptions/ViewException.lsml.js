f11f366364549c8362e62854e15374a831a83831
Lorry1 <jlorenc01@gmail.com>
2015-04-19 18:42:23 +0200

    #37 Screen: Exceptions

---
+++ Excellent.HTMLClient/Screens/Exceptions/ViewException.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewException.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Exception.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Exception." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
