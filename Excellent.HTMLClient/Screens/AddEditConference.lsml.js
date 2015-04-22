bf4584c0bf5b5010101b172c46386da386535ce3
weaper <xlenga01@gmail.com>
2015-04-22 14:36:00 +0200

    #35 Conference Settings: Opraveni preklepu, pridani elementu do Dates

---
+++ Excellent.HTMLClient/Screens/AddEditConference.lsml.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditConference.ContractLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Contract";
+};
+myapp.AddEditConference.NumberOfParticipantsLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Number Of Participants";
+};
+myapp.AddEditConference.ParticipantsLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Participants";
+};
+myapp.AddEditConference.LogoLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Logo";
+};
+myapp.AddEditConference.PosterLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Poster";
+};
+myapp.AddEditConference.VideoPresentationLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Video Presentation";
+};
+myapp.AddEditConference.PresentationListLabel_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "Presentation List";
+};
