7dd731e0093858ba0ed4ac2a50ec503e0bdb0f99
Filip Matys <filip.matys@artiisgroup.com>
2015-04-27 23:27:34 +0200

    #43 Changed icons and minor styling

--- Excellent.HTMLClient/Screens/Settings.lsml.js
+++ Excellent.HTMLClient/Screens/Settings.lsml.js
@@ -1,9 +1,9 @@
 ﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
 
 myapp.Settings.ShowBrowseUsers_postRender = function (element, contentItem) {
-    $(element).metroTile(contentItem, { imageUrl: 'Content/Images/user-logo-144.png' } /* TODO icon */);
+    $(element).metroTile(contentItem, { imageUrl: 'Content/Images/tile-users.png' } );
 };
 
 myapp.Settings.ShowBrowseConferences_postRender = function (element, contentItem) {
-    $(element).metroTile(contentItem, { imageUrl: 'Content/Images/user-logo-144.png' } /* TODO icon */);
+    $(element).metroTile(contentItem, { imageUrl: 'Content/Images/tile-conferences.png' } );
 };
