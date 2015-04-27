e70a1b19d261fefd144654be3cf1d83c8eaf3e48
Pavel Hrabec <xhrabe00@stud.fit.vutbr.cz>
2015-04-27 21:44:35 +0200

    #43 Screen Settings

---
+++ Excellent.HTMLClient/Screens/Settings.lsml.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.Settings.ShowBrowseUsers_postRender = function (element, contentItem) {
+    $(element).metroTile(contentItem, { imageUrl: 'Content/Images/user-logo-144.png' } /* TODO icon */);
+};
+
+myapp.Settings.ShowBrowseConferences_postRender = function (element, contentItem) {
+    $(element).metroTile(contentItem, { imageUrl: 'Content/Images/user-logo-144.png' } /* TODO icon */);
+};
---
+++ Excellent.HTMLClient/Scripts/metro-tile.js
@@ -0,0 +1,37 @@
+﻿(function ($) {
+    $.fn.metroTile = function (contentItem, options) {
+        $element = this;
+        var me = $.extend({
+            width: 200,
+            height: 200,
+            displayName: contentItem.model.displayName,
+            description: contentItem.model.description || '',
+            text : '',
+            element: this,
+            imageUrl: '',
+        }, options);
+
+        me.create = function () {
+            me.element.find('a').css({
+                'background-image': 'url(' + me.imageUrl + ')',
+                'background-repeat': 'no-repeat',
+                'background-position': 'center',
+                'width': '' + me.width + 'px',
+                'height': '' + me.height + 'px'
+            });
+            setTimeout(function () {
+                me.element.find('a').html(
+                '<div style="width:100%;height:100%;position:relative;">' +
+                '    <div class="text">' + me.text + '</div>' +
+                '    <div class="metroTile" >' +
+                '        <div class="background"></div>' +
+                '        <div class="title"><p>' + me.displayName + '</p></div>' +
+                '        <div class="description">' + me.description + '</div> ' +
+                '    </div>' +
+                '</div>');
+            });
+        };
+
+        me.create();
+    };
+}(jQuery));
