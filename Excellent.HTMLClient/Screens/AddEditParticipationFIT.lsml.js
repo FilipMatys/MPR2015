f0199ca958d3e387446477a25304db2bbed37707
weaper <xlenga01@gmail.com>
2015-04-30 14:21:42 +0200

    #58 - prvni nastrel

---
+++ Excellent.HTMLClient/Screens/AddEditParticipationFIT.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditParticipationFIT.ComId_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "444";
+};
+myapp.AddEditParticipationFIT.ComVAT_postRender = function (element, contentItem) {
+    // Write code here.
+    element.textContent = "474";
+};
