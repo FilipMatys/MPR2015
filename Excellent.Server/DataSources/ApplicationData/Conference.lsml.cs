5280ad84797d0cece45a88b00998bed22bb7a8df
weaper <xlenga01@gmail.com>
2015-04-30 23:01:50 +0200

    #35 - smazani zbytecneho kodu

--- Excellent.Server/DataSources/ApplicationData/Conference.lsml.cs
+++ Excellent.Server/DataSources/ApplicationData/Conference.lsml.cs
@@ -10,20 +10,5 @@ namespace LightSwitchApplication
 {
     public partial class Conference
     {
-        partial void Conference_Created()
-        {
-            Exception ex;
-            ex = new Exception();
-            throw new ArgumentException("A start-up parameter is required.");
-            Console.WriteLine("First display of filenames to the console:");
-            //MessageBox.Show("Do you want to save changes?");
-            this.Description = "Kocourkov";
-            /*Deadline ded = new Deadline();
-            ded.Description = "pokkku";
-            ded.Type = "Logo";
-            this.Deadlines.Add(ded);
-            this.Deadlines.AddNew();
-            */
-        }
     }
 }
--- Excellent.Server/DataSources/ApplicationData/_ApplicationDataService.lsml.cs
+++ Excellent.Server/DataSources/ApplicationData/_ApplicationDataService.lsml.cs
@@ -2,15 +2,5 @@
 {
     public partial class ApplicationDataService
     {
-        partial void Conferences_Updating(Conference entity)
-        {
-            /*
-            Deadline dedLogo = new Deadline();
-            dedLogo.Type = "Logo";
-            Deadline dedPoster = new Deadline();
-            dedLogo.Type = "Poster";
-            */
-            //this.;
-        }
     }
 }
