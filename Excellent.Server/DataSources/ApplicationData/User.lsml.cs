7b9034f424b49bf0f46d01ace822dc71fe5eb7e2
Lorry1 <jlorenc01@gmail.com>
2015-05-08 22:47:07 +0200

    #96 User validation

---
+++ Excellent.Server/DataSources/ApplicationData/User.lsml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class User
+    {
+        partial void Login_Validate(EntityValidationResultsBuilder results)
+        {
+            // results.AddPropertyError("<Error-Message>");
+            var dup = (from u in this.DataWorkspace.ApplicationData.Users
+                         where u.Id != this.Id && u.Login == this.Login
+                         select u);
+            if (dup.Cast<User>().Count() > 0)
+                results.AddPropertyError("Username " + this.Login + " already exists.");
+            return;
+        }
+
+        partial void Email_Validate(EntityValidationResultsBuilder results)
+        {
+            // results.AddPropertyError("<Error-Message>");
+            var dup = (from u in this.DataWorkspace.ApplicationData.Users
+                       where u.Id != this.Id && u.Email == this.Email
+                       select u);
+            if (dup.Cast<User>().Count() > 0)
+                results.AddPropertyError("Email " + this.Email + " already exists.");
+            return;
+        }
+    }
+}
